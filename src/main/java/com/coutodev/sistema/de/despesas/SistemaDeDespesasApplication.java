f7002687a2535d52a1ca154e8e2a8071c75733ad
caua <cauacouto022@gmail.com>
2025-11-27 22:44:57 -0300

    first commit

---
+++ src/main/java/com/coutodev/sistema/de/despesas/SistemaDeDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.coutodev.sistema.de.despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeDespesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coutodev/sistema/de/despesas/SistemaDeDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutodev.sistema.de.despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
